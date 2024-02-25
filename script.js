d05df7788b1746354ed3304e40ec59a6902e65c4
rayssa <limarayssa046@gmail.com>
2024-02-25 16:33:52 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+console.log("Olá mundo!");
+
+   var nome = "Rayssa";
+   var nome2 = "Lima";
+   var idade = "20";
+   var cor = "Preto";
+   var periodo = "5";
+//o que vai aparecer escrito na pagina, mas eu preciso chamar.
+
+   var nomeFinal = nome+nome2;
+// consigo somar palavras ou numeros, preciso descrever o que quero chamar.
+   
+
+   document.write(nomeFinal);
+
+   document.write(idade); 
+
+   document.write(periodo);
+//estou chamando o que eu quero para a pagina principal.
+
+
+//ctrl + s = salvar no Visual Studio Code
+//F5 = aparecer na pagina web
+// ctrl + alt + N
+
+
+
